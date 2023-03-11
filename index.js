88cdb49976bc34132f9ed1b8a46553d456af433b
paul kabuu <kongkabuu9@gmail.com>
2023-03-11 16:04:56 +0300

    done assingmnet

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee ={
+    name:"kabuu paul" ,
+    streetAdress: "0045",
+};
+
+function updateEmployeeWithKeyAndValue(obj,key, value){
+
+let  staff ={...obj};
+staff[key] = value;
+return staff;
+
+
+return staff;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key ,value){
+
+obj[key]= value;
+return obj;
+
+}
+
+function deleteFromEmployeeByKey(obj, key){
+
+let staff ={...obj};
+delete staff[key];
+return staff;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+return obj;
+}
